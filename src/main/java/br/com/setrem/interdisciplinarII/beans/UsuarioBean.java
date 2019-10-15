941f690344557fd19a2bb9f61fac1dc553a45a7c
Lucas Alf <lukinhasalf@gmail.com>
2019-10-14 21:46:56 -0300

    Correções na segurança do login

--- src/main/java/br/com/setrem/interdisciplinarII/beans/UsuarioBean.java
+++ src/main/java/br/com/setrem/interdisciplinarII/beans/UsuarioBean.java
@@ -11,9 +11,9 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import br.com.setrem.interdisciplinarII.model.Usuario;
 import br.com.setrem.interdisciplinarII.repository.UsuarioRepository;
 
@@ -24,13 +24,47 @@ public class UsuarioBean implements Serializable {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private HttpServletRequest request;
+
+    public Usuario usuario = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
+            .get("usuario");
+
     public UsuarioBean() {
+    }
+
+    public String Sair() {
+        usuario = null;
+        if (FacesContext.getCurrentInstance().getExternalContext().getSessionMap().containsKey("usuario")) {
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove("usuario");
+        }
+        return "/index.xhtml?faces-redirect=true";
+    }
+
+    public String VerificaLogin() {
+        String urlAtual = request.getRequestURL().toString();
+        if (usuario == null) {
+            if (urlAtual == null || !urlAtual.contains("index.xhtml")) {
+                return "/index.xhtml?faces-redirect=true";
+            } else {
+                return "";
+            }
+        } else {
+            if (urlAtual.contains("index.xhtml") || urlAtual.endsWith("/")) {
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Autenticado com usuário da sessão. ("+usuario.getEmail()+")"));
+                return "/home.xhtml?faces-redirect=true";
+            }
+            return "";
+        }
     }
 
     public String Login(String email, String senha) {
         Usuario usuario = usuarioRepository.login(email, senha);
         if (usuario != null) {
-            System.out.println("-> Login: " + usuario.getEmail());
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuario", usuario);
+            this.usuario = usuario;
+            System.out.println("-> Login: " + usuario.getEmail() + ". Usuário salvo na sessão!");
             return "/home.xhtml?faces-redirect=true";
         } else {
             System.out.println("-> Usuario não encontrado!");
