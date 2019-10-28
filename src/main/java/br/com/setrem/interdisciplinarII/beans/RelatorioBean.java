c87659295d7575fa164aa2635b5ab61170781145
Eliane Fiepke <eliane_fiepke@hotmail.com>
2019-10-27 21:21:30 -0300

    Jasper Reports

--- src/main/java/br/com/setrem/interdisciplinarII/beans/RelatorioBean.java
+++ src/main/java/br/com/setrem/interdisciplinarII/beans/RelatorioBean.java
@@ -5,15 +5,23 @@
  */
 package br.com.setrem.interdisciplinarII.beans;
 
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.result.Output;
 import org.primefaces.PrimeFaces;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -22,6 +30,18 @@ import br.com.setrem.interdisciplinarII.model.FiltroRelatorio;
 import br.com.setrem.interdisciplinarII.model.Relatorio;
 import br.com.setrem.interdisciplinarII.repository.FiltroRelatorioRepository;
 import br.com.setrem.interdisciplinarII.repository.RelatorioRepository;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.Exporter;
+import net.sf.jasperreports.export.ExporterOutput;
+import net.sf.jasperreports.export.OutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
 
 @Named(value = "relatorioBean")
 @SessionScoped
@@ -36,6 +56,7 @@ public class RelatorioBean implements Serializable {
     private List<Relatorio> listaRelatorio = new ArrayList<Relatorio>();
     private List<FiltroRelatorio> listaFiltroRelatorio = new ArrayList<FiltroRelatorio>();
     private List<FiltroRelatorio> listaFiltroRelatorioSelecionados = new ArrayList<FiltroRelatorio>();
+    private Connection conexao = null;
 
     public String getDescricaoBusca() {
         return descricaoBusca;
@@ -90,9 +111,67 @@ public class RelatorioBean implements Serializable {
                         "Nenhum filtro encontrado para o relatório.");
                 FacesContext context = FacesContext.getCurrentInstance();
                 context.addMessage(null, fm);
+                this.listaFiltroRelatorioSelecionados = new ArrayList<FiltroRelatorio>();
+                ImprimirRelatorio(relatorioId);
+            } else {
+                PrimeFaces.current().executeScript("$('#impressaoModal').modal('show');");
             }
-            PrimeFaces.current().executeScript("$('#impressaoModal').modal('show');");
         }
     }
 
+    public void ImprimirRelatorio(int relatorioId) {
+        Optional<Relatorio> relatorio = relatorioRepository.findById(relatorioId);
+        if (relatorio.isPresent()) {
+            // Abre conexão com o banco
+            try {
+                String url = "jdbc:postgresql://ec2-50-19-95-77.compute-1.amazonaws.com:5432/d43v5g6nfkagp";
+                String usuario = "pnbqtudcpohwgn";
+                String senha = "c3265949766568e312fdc45e956de4aa8eec60121034cf0871821674bce06027";
+                this.conexao = DriverManager.getConnection(url, usuario, senha);
+            } catch (Exception e) {
+                FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro",
+                        "Não foi possível abrir uma conexão com o banco de dados!");
+                FacesContext context = FacesContext.getCurrentInstance();
+                context.addMessage(null, fm);
+            }
+            // JASPER
+            try {
+                // Compila relatório
+                JasperPrint print = JasperFillManager.fillReport("reports/" + relatorio.get().getNome() + ".jasper",
+                        null, conexao);
+
+                // Exporta relatório
+                HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance()
+                        .getExternalContext().getResponse();
+                OutputStream fos = response.getOutputStream();
+                OutputStreamExporterOutput outpuStream = new SimpleOutputStreamExporterOutput(fos);
+                JRPdfExporter exporter = new JRPdfExporter();
+                exporter.setExporterInput(new SimpleExporterInput(print));
+                exporter.setExporterOutput(outpuStream);
+                SimplePdfExporterConfiguration conf = new SimplePdfExporterConfiguration();
+                exporter.setConfiguration(conf);
+                exporter.exportReport();
+
+            } catch (Exception e) {
+                FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro",
+                        "Ocorreu um erro ao imprimir o relatório!");
+                FacesContext context = FacesContext.getCurrentInstance();
+                context.addMessage(null, fm);
+            } finally {
+                // Fecha a conexão com o banco
+                try {
+                    this.conexao.close();
+                } catch (Exception e) {
+                    FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro",
+                            "Não foi possível fechar a conexão com o banco de dados!");
+                    FacesContext context = FacesContext.getCurrentInstance();
+                    context.addMessage(null, fm);
+                }
+            }
+        } else {
+            FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Relatório não encontrado!");
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, fm);
+        }
+    }
 }
