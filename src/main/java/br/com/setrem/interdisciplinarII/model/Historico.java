6ebb9208e18fed7bc07c87b1af74f5b711039c9a
Luan Dopke <luandopke@gmail.com>
2019-11-05 21:49:35 -0300

    Começado histórico

---
+++ src/main/java/br/com/setrem/interdisciplinarII/model/Historico.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.setrem.interdisciplinarII.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+
+/**
+ *
+ * 
+ */
+@Entity(name = "marca")
+
+public class Historico implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "historico")
+    private String historico;
+
+    public Historico() {
+    }
+
+    public Historico(Integer id) {
+        this.id = id;
+    }
+
+    public Historico(Integer id, String historico, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+        this.historico = historico;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /*@XmlTransient
+    public Collection<Produto> getProdutoCollection() {
+        return produtoCollection;
+    }
+
+    public void setProdutoCollection(Collection<Produto> produtoCollection) {
+        this.produtoCollection = produtoCollection;
+    }*/
+
+   
+
+    @Override
+    public String toString() {
+        return "br.com.setrem.interdisciplinarII.model.Marca[ id=" + id + " ]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+        result = prime * result + ((historico == null) ? 0 : historico.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Historico other = (Historico) obj;
+        if (descricao == null) {
+            if (other.descricao != null)
+                return false;
+        } else if (!descricao.equals(other.descricao))
+            return false;
+        if (historico == null) {
+            if (other.historico != null)
+                return false;
+        } else if (!historico.equals(other.historico))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getHistorico() {
+        return historico;
+    }
+
+    public void setHistorico(String historico) {
+        this.historico = historico;
+    }
+
+}
